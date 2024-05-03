65ef708677e09a2946968f2eb6da5fd0a1a88fc9
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2024-05-03 22:13:04 +0300

    [Task-5] feat: Task 5 completed

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""A module for defining sum_list"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Calculates the total sum"""
+    return float(sum(input_list))
