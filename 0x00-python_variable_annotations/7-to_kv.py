7a654e8c03af55c494ed7100a2c0937e130f1c93
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2024-05-03 22:27:59 +0300

    [Task-7] feat: Task 7 completed

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -2,6 +2,7 @@
 """A module for defining to_kv"""
 from typing import Tuple, Union
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Make a tuple out of k and v"""
     return (k, float(v**2))
