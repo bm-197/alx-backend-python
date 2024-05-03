beac8f7d0383516f832a0c6f4cfe6fa67b57f0fa
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2024-05-03 21:58:08 +0300

    [Task-0, Task-1] feat: Task 0 and Task 1 completed

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""A module for defining add method"""
+
+
+def add(a: float, b: float) -> float:
+	"""Adds a and b"""
+	return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A module defining concat method"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concatinates str1 and str2"""
+    return str1 + str2
