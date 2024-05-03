338adc57bc3441d455ae2d9ecee0621e154bad7e
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2024-05-03 22:28:48 +0300

    [Task-6] feat: Task 6 completed

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-"""A module for defining sum_mixed_list"""
+'''Task 6's module.
+'''
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_list: List[Union[str, float]]) -> float:
-    """Calculates the total sum"""
-    return float(sum(mxd_list))
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    '''Computes the sum of a list of integers and floating-point numbers.
+    '''
+    return float(sum(mxd_lst))
