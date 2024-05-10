90fad1dcfa5963ef1c85d339b3320e68bb6c0632
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2024-05-10 10:54:39 +0300

    fixed typing.Generator parameter requirment

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,10 +6,9 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float]:
+async def async_generator() -> Generator[float, None, None]:
     '''Generates a sequence of 10 numbers.
     '''
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.random() * 10
-        
