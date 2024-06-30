d8e7b1702a3cb320f737baecafbb08c05c26a54b
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2024-06-30 12:31:35 +0300

    [Task 1] improvment(test): improve utils.py to test exception raised by access_nested_map

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,3 +19,13 @@ class TestAccessNestedMap(unittest.TestCase):
         """ Tests the access_nested_map's output.
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError)
+    ])
+    def test_access_nested_map_exception(self, nested_map: Dict, path: Tuple[str], exception: Exception):
+        """ Test the access_nested_map's exceptions.
+        """
+        with self.assertRaises(exception):
+            access_nested_map(nested_map, path)
