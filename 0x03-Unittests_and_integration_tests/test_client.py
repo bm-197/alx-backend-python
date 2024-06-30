b369e46a4ee00fb78bdfacb48e541d04091b0b31
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2024-06-30 15:39:04 +0300

    [Task 4] improvment(test): improve test_client to test the _public_repos_url

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -21,3 +21,12 @@ class TestGithubOrgClient(unittest.TestCase):
         github_org_client = GithubOrgClient(org)
         self.assertEqual(github_org_client.org(org), response)
         mock_fn.assert_called_once_with("https://api.github.com/orgs/{}".format(org))
+    
+    def test_public_repos_url(self) -> None:
+        """Tests the _public_repos_url property."""
+        with patch("client.GithubOrgClient.org", new_callable=PropertyMock,) as mock_org:
+            mock_org.return_value = {'repos_url': "https://api.github.com/users/google/repos"}
+            self.assertEqual(
+                GithubOrgClient("google")._public_repos_url,
+                "https://api.github.com/users/google/repos",
+            )
