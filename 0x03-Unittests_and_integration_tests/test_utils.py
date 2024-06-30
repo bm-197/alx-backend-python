10dbc9d470db71ad29c2a093f816a56dcf7c71cf
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2024-06-30 15:02:16 +0300

    [Task 3] improvment(test): improve test_utils.py to test the memoized function

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,7 +5,7 @@ import unittest
 from typing import Dict, Tuple, Union
 from unittest.mock import Mock, patch
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -47,3 +47,24 @@ class TestGetJson(unittest.TestCase):
         with patch('utils.requests.get', return_value=mock_response) as mock_get:
             self.assertEqual(get_json(test_url), test_payload)
             mock_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """ Tests the memoize function. 
+    """
+    def test_memoize(self) -> None:
+        """ Test memoize's output.
+        """
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method', return_value=lambda: 42) as memo_fn:
+            testclass = TestClass()
+            self.assertEqual(testclass.a_property(), 42)
+            self.assertEqual(testclass.a_property(), 42)
+            memo_fn.assert_called_once()
